87de7afb9c8e8008f3b652158e3c3d87de9d05fd
Glimmer <lige123456li@gmail.com>
2026-07-12 04:51:53 +0800

    Use Supabase anon key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MBAPPE_SITE_CONFIG = {
   supabaseUrl: "https://dqkhffhxbftpamtqbfox.supabase.co",
-  supabaseAnonKey: "sb_publishable_2cNNiqUDxvmGt5-F1M81Wg_pfECeTDK",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxa2hmZmh4YmZ0cGFtdHFiZm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NzQyMDEsImV4cCI6MjA5OTM1MDIwMX0.KMCEL_Zh7tAVC4NkibxfZHkDmliiiiByp2WF7V-sTpw",
   tableName: "declarations"
 };
